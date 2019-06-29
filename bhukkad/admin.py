2d2eac4509c419ee623c1fa1e0f784ef3faecdc8
alok <alok@helpshift.com>
2019-06-29 15:30:19 +0530

    Add readability

--- bhukkad/admin.py
+++ bhukkad/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import User, Food, Order
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Food)
+admin.site.register(Order)
--- bhukkad/models.py
+++ bhukkad/models.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
-
+@python_2_unicode_compatible
 class User(models.Model):
     user_name = models.CharField(max_length=200)
     is_active = models.BooleanField(True)
 
+    def __str__(self):
+        return '%s %d' % (self.user_name, self.is_active)
+    
+
+@python_2_unicode_compatible
 class Food(models.Model):
     food_name = models.CharField(max_length=200)
     is_served = models.BooleanField(True)
 
+    def __str__(self):
+        return '%s %d' % (self.food_name, self.is_served)
+
+@python_2_unicode_compatible
 class Order(models.Model):
     user = models.ForeignKey(User)
     food = models.ForeignKey(Food)
@@ -22,6 +31,5 @@ class Order(models.Model):
     instructions = models.CharField(max_length=200)
     time = models.DateTimeField(auto_now_add=True)
 
-class OrderHistory(models.Model):
-    order = models.ForeignKey(Order)
-    user = models.ForeignKey(User)
+    def __str__(self):
+        return '%s %s %d %d %s %s' % (self.user.user_name, self.food.food_name, self.quantity, self.status, self.instructions, self.time)
